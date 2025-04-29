310ac7fed043a025a25fe7bd3638ca5586b9ee97
patrickmao <patrickmao@live.com>
2025-04-28 20:32:32 -0500

    simplify network code

--- network/network.go
+++ network/network.go
@@ -20,13 +20,10 @@ type Network struct {
 	peers     []*types.Peer
 	clients   []types.ConsensusRPCClient
 
-	mu           sync.Mutex
-	inboundCond  *sync.Cond
-	outboundCond *sync.Cond
+	mu          sync.Mutex
+	inboundCond *sync.Cond
 
-	inbound  map[string]*types.Message
 	deferred map[string]*types.Message
-	outbound map[string]*types.Message
 }
 
 type HandleMsgFunc func(msg *types.Message) (shouldDefer bool)
@@ -36,27 +33,22 @@ func NewNetwork(myIndex uint32, key *ecdsa.PrivateKey, handleMsg HandleMsgFunc) 
 		idx:       myIndex,
 		key:       key,
 		handleMsg: handleMsg,
-		inbound:   make(map[string]*types.Message),
 		deferred:  make(map[string]*types.Message),
-		outbound:  make(map[string]*types.Message),
 	}
 	n.inboundCond = sync.NewCond(&n.mu)
-	n.outboundCond = sync.NewCond(&n.mu)
 	return n
 }
 
 func (n *Network) Start() {
 	go n.startRPC()
 	go n.dialPeers()
-	go n.processDeferred()
-	go n.processInbound()
-	n.processOutbound()
+	n.processDeferred()
 }
 
 // Broadcast sends the msg to all nodes in the network asynchronously.
 // NOTE: this function returns immediately without waiting for the other nodes to respond
 func (n *Network) Broadcast(msg *types.Message) {
-	n.ingestOutbound([]*types.Message{msg})
+	n.doBroadcast([]*types.Message{msg})
 }
 
 func (n *Network) dialPeers() {
@@ -68,21 +60,6 @@ func (n *Network) dialPeers() {
 			continue
 		}
 		n.clients = append(n.clients, types.NewConsensusRPCClient(cc))
-	}
-}
-func (n *Network) processInbound() {
-	for {
-		n.mu.Lock()
-		for len(n.outbound) == 0 {
-			n.inboundCond.Wait()
-		}
-		for id, msg := range n.inbound {
-			retry := n.handleMsg(msg)
-			if retry {
-				n.deferred[id] = msg
-			}
-		}
-		n.mu.Unlock()
 	}
 }
 
@@ -103,37 +80,10 @@ func (n *Network) processDeferred() {
 	}
 }
 
-func (n *Network) processOutbound() {
-	for {
-		n.mu.Lock()
-		for len(n.outbound) == 0 {
-			n.outboundCond.Wait()
-		}
-		msgs := make([]*types.Message, 0, len(n.outbound))
-		for _, msg := range n.outbound {
-			msgs = append(msgs, msg)
-		}
-		n.mu.Unlock()
-		n.doBroadcast(msgs)
-	}
-}
-
 func (n *Network) ingestInbound(msgs []*types.Message) {
-	n.mu.Lock()
 	for _, msg := range msgs {
-		n.inbound[string(msg.Hash())] = msg
+		go n.handleMsg(msg)
 	}
-	n.mu.Unlock()
-	n.inboundCond.Broadcast()
-}
-
-func (n *Network) ingestOutbound(msgs []*types.Message) {
-	n.mu.Lock()
-	for _, msg := range msgs {
-		n.outbound[string(msg.Hash())] = msg
-	}
-	n.mu.Unlock()
-	n.outboundCond.Broadcast()
 }
 
 func (n *Network) doBroadcast(msgs []*types.Message) {
@@ -144,18 +94,14 @@ func (n *Network) doBroadcast(msgs []*types.Message) {
 		Sig:       n.sign(_msgs),
 	}
 
-	var wg sync.WaitGroup
 	for i, client := range n.clients {
-		wg.Add(1)
 		go func() {
-			defer wg.Done()
 			_, err := client.Send(context.Background(), envelope)
 			if err != nil {
 				log.Errorf("failed to send to peer %d: %s", i, err.Error())
 			}
 		}()
 	}
-	wg.Wait()
 
 	return
 }
